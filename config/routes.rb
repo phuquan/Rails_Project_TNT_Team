3ff98e60ca06180780180161443ddd2180ca13f0
White <cooro1994@gmail.com>
2016-03-28 06:51:18 +0000

    add image routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get 'users/show'
 
   devise_for :users
+  resources :images
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
