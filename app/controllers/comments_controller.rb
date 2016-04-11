77eafc01e4be2b260d5309b09169ca3e58c3b468
DA LV <dalv0911@gmail.com>
2016-04-11 09:05:36 +0000

    add comment using jquery

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,23 @@
 class CommentsController < ApplicationController
   def create
+    @comment = Comment.new(comment_params)
+    @image = Image.find(@comment.image_id)
+    respond_to do |format|
+      if @comment.save
+        @new_comment = Comment.new
+        format.html  { redirect_to(:back, :notice => 'Comment was successfully added.') }
+        format.js
+      else
+        format.html  { render :action => "new" }
+      end
+    end
   end
 
   def destroy
+  end
+  
+  private
+  def comment_params
+      params.require(:comment).permit(:context, :image_id, :user_id)
   end
 end
